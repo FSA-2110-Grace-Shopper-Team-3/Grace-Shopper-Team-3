9db5690f06b1e4338eea682fcbf590e12706dbed
David <dwoo1016@gmail.com>
2022-02-05 15:49:01 -0500

    added SingleProduct

---
+++ client/components/SingleProduct.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+
+const SingleProduct = () => {
+
+  const { id }= useParams();
+  const singleProduct = useSelector((state) => state.products.find((product) => product.id === id * 1)) || {};
+
+  return (
+    <div>
+      <ul>
+        <li>{singleProduct.brand}</li>
+        <li>{singleProduct.model}</li>
+        <li>{singleProduct.price}</li>
+        <li>{singleProduct.description}</li>
+        <li>{singleProduct.category}</li>
+      </ul>     
+    </div>
+  )
+}
+
+export default SingleProduct
