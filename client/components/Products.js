97af1051482859fa9db31a178563db3a53718a5b
k-zammito <kzammito@gmail.com>
2022-02-06 13:39:11 -0500

    edited products page

--- client/components/Products.js
+++ client/components/Products.js
@@ -1,26 +1,25 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 const Products = () => {
-
   const products = useSelector((state) => state.products);
 
   return (
     <div>
-      {
-        products.map((product) => {
-          return (
-            <div key={product.id}>
-              {
-                <Link to={`/products/${product.id}`}>{product.brand}</Link>
-              }
-            </div>
-          )
-        })
-      }
+      {products.map((product) => {
+        return (
+          <div key={product.id}>
+            {
+              <Link to={`/products/${product.id}`}>
+                {product.category} - {product.brand} - {product.model}
+              </Link>
+            }
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
