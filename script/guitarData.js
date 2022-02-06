60fa0bc4a82064af5732512ed363da2112ef9537
k-zammito <kzammito@gmail.com>
2022-02-06 13:24:38 -0500

    added some guitar data

--- script/guitarData.js
+++ script/guitarData.js
@@ -1,20 +1,48 @@
 const guitars = [
   {
-    brand: 'Fender',
-    model: 'Acoustasonic',
-    price: 1999.99,
-    description:
-      'Guitarists of diverse styles and tastes are finding their voice in the new Fender Acoustasonic Telecaster.',
-    category: 'Guitar',
-    img: 'https://sc1.musik-produktiv.com/pic-010128323xxl/fender-acoustasonic-jazzmaster-arctic-white.jpg',
-  },
-  {
     brand: 'Gibson',
     model: 'J-45',
-    price: 2849.99,
+    price: 3499.0,
     description:
       'The J-45 has been a staple and top-seller for Gibson since its debut in 1942.',
     category: 'Guitar',
+    img: 'https://c1.zzounds.com/media/productmedia/fit,600by800/quality,85/1_Full_Straight_Front_44410-450b52d2ce9e2cee69ef556f54a18ed9.jpg',
+  },
+  {
+    brand: 'Gibson',
+    model: 'Hummingbird Studio',
+    price: 3999.0,
+    description:
+      "The unmistakable Gibson Hummingbird is followed by the masses as one of the most remarkable acoustic guitars ever built. The Hummingbird Original blends vintage appointments and today's modern, high-quality build construction.",
+    category: 'Guitar',
+    img: 'https://c1.zzounds.com/media/productmedia/fit,2018by3200/quality,85/1_Full_Straight_Front_NA-b00f4e68d3bf5fd6392445d6ecfcc523.jpg',
+  },
+  {
+    brand: 'Gibson',
+    model: 'Dove',
+    price: 4499.0,
+    description:
+      'This famous flame maple-backed square-shoulder introduced in the early 60s is intertwined with one of the most revolutionary decades in popular music history.',
+    category: 'Guitar',
+    img: 'https://c1.zzounds.com/media/productmedia/fit,2018by3200/quality,85/1_Full_Straight_Front_NA-2bd48c0458902ff4cfe26ce7f7aadbd9.jpg',
+  },
+  {
+    brand: 'Gibson',
+    model: 'L-00',
+    price: 2799.0,
+    description:
+      'Gibson was a pioneer in the development of small body guitars in the beginning of the 1900s with its "L-Series Small Body" acoustics. These smaller Gibsons deliver a tonality that is best described in Gibson\'s 1934 catalog as a sound of "perfect balance."',
+    category: 'Guitar',
+    img: 'https://c1.zzounds.com/media/productmedia/fit,2018by3200/quality,85/1_Full_Straight_Front_NA-0ad92ebb03b36e4fc9a67cfda2ff750e.jpg',
+  },
+  {
+    brand: 'Gibson',
+    model: 'Generation G-00 ',
+    price: 999.0,
+    description:
+      "The perfect personal parlor guitar, the Gibson G-00 is equipped with a Gibson Player Port. The Player Port is an original Gibson concept from the early 1960s that has been refined by Gibson's acoustic luthier team to deliver a truly revolutionary sonic improvement that adds a new dimension to the sound. ",
+    category: 'Guitar',
+    img: 'https://c1.zzounds.com/media/productmedia/fit,2018by3200/quality,85/1_Full_Straight_Front_NA-12b4a1a99b13c249e24954953e3e2cc5.jpg',
   },
 ];
 
