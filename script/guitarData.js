602576a26e8a4d333bb3645a997deee21f24902b
k-zammito <kzammito@gmail.com>
2022-02-06 13:58:39 -0500

    added more gtr data

--- script/guitarData.js
+++ script/guitarData.js
@@ -44,6 +44,42 @@ const guitars = [
     category: 'Guitar',
     img: 'https://c1.zzounds.com/media/productmedia/fit,2018by3200/quality,85/1_Full_Straight_Front_NA-12b4a1a99b13c249e24954953e3e2cc5.jpg',
   },
+  {
+    brand: 'Martin',
+    model: 'D-45',
+    price: 10499.0,
+    description:
+      "Launched in 1906, Britain's HMS Dreadnought was the first all-big-gun battleship and the first to use steam-turbine propulsion. Larger, faster, quieter, and more deadly than any warship that had come before, it ushered in the modern era of naval warfare. ",
+    category: 'Guitar',
+    img: 'https://media.sweetwater.com/api/i/f-webp__q-82__ha-009c24e8a7b93cb2__hmac-bc236ceb244efcc63366a244f349deede6ac67c0/images/closeup/750-D45MD_front.jpg',
+  },
+  {
+    brand: 'Martin',
+    model: '000-42 Modern Deluxe',
+    price: 7199.0,
+    description:
+      "Martin's 000-42 Modern Deluxe is a refreshing take on the classic auditorium body style, blending old-world style with modern refinements. Its Sitka spruce Vintage Tone System (VTS) top has been torrefied for maximum projection and harmonic excitement. The dovetail neck joint reinforces the classic Martin tone.",
+    category: 'Guitar',
+    img: 'https://media.sweetwater.com/api/i/f-webp__q-82__ha-f674c6de2c3b617d__hmac-9731bf49e09a5c47887be7fa378fbe67c971a9cb/images/closeup/750-00042MD_front.jpg',
+  },
+  {
+    brand: 'Martin',
+    model: '000-18 Modern Deluxe',
+    price: 3599.0,
+    description:
+      "Martin's 000-18 Modern Deluxe is a refreshing take on the classic auditorium body style, blending old-world style with modern refinements. Its Sitka spruce Vintage Tone System (VTS) top has been torrefied for maximum projection and harmonic excitement.",
+    category: 'Guitar',
+    img: 'https://media.sweetwater.com/api/i/f-webp__q-82__ha-042e303cfc8e2e18__hmac-6f7c5c30d07049088cf44bad6c7a0bc912dc9239/images/closeup/750-00018MD_front.jpg',
+  },
+  {
+    brand: 'Martin',
+    model: 'SC-13E Special',
+    price: 1799.0,
+    description:
+      'Looking for a high-performance acoustic-electric guitar? The Martin Road Series SC-13E Special delivers the goods, eliminating the limitations of the conventional cutaway acoustic. This game-changing A/E exhibits a gorgeous modern acoustic sound — direct, punchy, and dynamic — with incredible volume and projection.',
+    category: 'Guitar',
+    img: 'https://media.sweetwater.com/api/i/f-webp__q-82__ha-e7f5aad9b4cec698__hmac-efecbf3e85262ed7f5c535e9cc267d2627ab8eac/images/closeup/750-SC13ESp_front.jpg',
+  },
 ];
 
 module.exports = guitars;
