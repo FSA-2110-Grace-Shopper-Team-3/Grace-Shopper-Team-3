b5a05e98723de325fbb3a6dde809ccf11d4be8b0
David <dwoo1016@gmail.com>
2022-02-06 10:06:35 -0500

    edit dwoo.html

--- server/db/models/Order.js
+++ server/db/models/Order.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Cart = () => {
+const Order = () => {
   return (
     <div>
       Hello world!
@@ -8,4 +8,4 @@ const Cart = () => {
   )
 }
 
-export default Cart
+export default Order
