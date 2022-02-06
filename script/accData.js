25fcb78b51d4295d2eac9e97796887e80bedf5ee
Louis J Rabeno <lrabeno@gmail.com>
2022-02-06 14:59:45 -0500

    extra data completed

---
+++ script/accData.js
@@ -0,0 +1,49 @@
+const accesories = [
+  {
+    brand: 'Vic Firth Sticks',
+    model: 'American Classic',
+    price: 10.99,
+    description:
+      'Vic Firth American Classic drumsticks are the choice of top players across the world.',
+    category: 'Accesory',
+    img: 'https://d1aeri3ty3izns.cloudfront.net/media/39/395750/600/preview_1.jpg',
+  },
+  {
+    brand: 'Ernie Ball Strings',
+    model: 'Earthwood Bronze 80/20',
+    price: 12.99,
+    description:
+      'Ernie Ball Earthwood 80/20 Bronze Acoustic Guitar Strings are made from 80% copper, 20% zinc wire wrapped around hex shaped brass plated steel core wire. These acoustic guitar strings provide a crisp, ringing sound with pleasing overtones.',
+    category: 'Accesory',
+    img: 'https://cdn.shopify.com/s/files/1/0217/5614/products/EB2003-opt_2000x.jpg?v=1600127250',
+  },
+  {
+    brand: 'Remp ET-7110-00',
+    model: 'Lollipop',
+    price: 100000000,
+    description:
+      'This is the best product ever! Only real ones get our special edition lollipop, but only if you can afford it!',
+    category: 'Accesory',
+    img: 'https://m.media-amazon.com/images/I/41tUsIabwuL._AC_.jpg',
+  },
+  {
+    brand: 'NS Design Cello Bow',
+    model: 'A-700',
+    price: 200,
+    description:
+      'Crafted using advanced molding techniques, the NS Design Cello Composite Bow is constructed of carbon fiber and other modern materials. This futuristic design delivers a new level of performance — way beyond that of traditional wood construction.',
+    category: 'Accesory',
+    img: 'https://cdn.images.fecom-media.com/FE00015313/images/HE1802546_1426061-HOP-MUS-P01.jpg',
+  },
+  {
+    brand: 'Dunlop Guitar Picks',
+    model: 'PVP101',
+    price: 20,
+    description:
+      'The Dunlop PVP101 Pick Variety Pack is ideal if you are always losing picks, or just looking to try out a decent selection of picks.',
+    category: 'Accesory',
+    img: 'https://media.sweetwater.com/api/i/q-85__ha-133f954120053ea7__hmac-227aa1f63b9b71380ce973bbd3a750d4aa2e37d3/images/items/1800/PVP101-xlarge.jpg',
+  },
+];
+
+module.exports = accesories;
---
+++ script/pianoData.js
@@ -0,0 +1,49 @@
+const piano = [
+  {
+    brand: 'Yamaha',
+    model: 'Arias',
+    price: 125000.99,
+    description:
+      "The Yamaha Arius YDP-164 brings together true piano sound and feel, for an outstanding playing experience. Yamaha incorporated their high-tech sampling system into the YDP-164, so you get the rich sound of Yamaha's flagship 9-foot CFX concert grand.",
+    category: 'Piano',
+    img: 'https://media.sweetwater.com/api/i/q-82__ha-7fbafc2aa5b251dd__hmac-9107d146513a9ab9986cd89670a54ae3d1db90c6/images/items/750/YDP164RW-large.jpg',
+  },
+  {
+    brand: 'Steinway',
+    model: 'Concert Grand',
+    price: 250000.99,
+    description:
+      'A lovely 1866 Steinway & Sons concert grand piano with rosewood case. The elegantly carved Rococo-style scrolled legs and ornate foliate details are ebonized, producing a striking contrast against the case, while the pierced and carved music rack and pedal lyre further display the masterful construction and design elements.',
+    category: 'Piano',
+    img: 'https://www.liveauctioneers.com/news/wp-content/uploads/2019/04/74091.jpg',
+  },
+  {
+    brand: 'Roland',
+    model: 'RP-701',
+    price: 250000.99,
+    description:
+      "Equipped with Roland's renowned SuperNATURAL sound engine, you get an intimate and customizable connection with your piano. Each note can be shaped to respond to your touch and dynamics with the tone and harmonic accuracy you desire.",
+    category: 'Piano',
+    img: 'https://media.sweetwater.com/api/i/q-85__ha-0a744bee1b95f853__hmac-fa6bfc6aaf90d3e8f7878239970fee9a960f64a7/images/items/1800/RP701WH-xlarge.jpg',
+  },
+  {
+    brand: 'Kawai',
+    model: 'CA49',
+    price: 350000.99,
+    description:
+      'If performance, elegance, and value are your top concerns, We have an excellent suggestion: the Kawai CA49. With its trim footprint and attractive finish, the CA49 will grace any room.',
+    category: 'Piano',
+    img: 'https://m.media-amazon.com/images/I/71aY1JD8FfL._AC_SX466_.jpg',
+  },
+  {
+    brand: 'Steinway',
+    model: 'Luxury',
+    price: 350000.99,
+    description:
+      'Not only will it sound exquisite, but it looks amazing - the perfect addition to any home. Steinway has a global reputation for quality and value. Uncompromising care and craftsmanship go into each piano built at their state-of-the-art facilities.',
+    category: 'Piano',
+    img: 'https://pianopiano.com/wp-content/uploads/2015/08/Screen-Shot-2019-12-10-at-2.39.05-PM.png',
+  },
+];
+
+module.exports = piano;
