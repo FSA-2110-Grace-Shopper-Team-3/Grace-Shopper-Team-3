bc9eb7dd5d3f780d34662e85f27b3be96ec34832
David <dwoo1016@gmail.com>
2022-02-07 14:06:45 -0500

    edit Models

---
+++ script/userData.js
@@ -0,0 +1,37 @@
+const users = [
+  {
+    username: 'louis',
+    password: '123',
+    email: 'louis@gmail.com',
+    address: '123 louis lane',
+    phoneNumber: '555-555-5555',
+    paymentInfo: '1234 6789 0191 7474',
+  },
+  {
+    username: 'david',
+    password: '123',
+    email: 'david@gmail.com',
+    address: '456 david street',
+    phoneNumber: '123-123-1234',
+    paymentInfo: '1234 6675 0861 4747',
+  },
+  {
+    username: 'kenny',
+    password: '123',
+    email: 'kenny@gmail.com',
+    address: '789 kenny court',
+    phoneNumber: '222-234-2323',
+    paymentInfo: '1234 6655 5656 5656',
+  },
+  {
+    username: 'admin',
+    password: 'admin',
+    email: 'admin@gmail.com',
+    address: '789 admin court',
+    phoneNumber: '911-119-9191',
+    paymentInfo: '1234 6655 5656 5657',
+    isAdmin: true,
+  },
+];
+
+module.exports = users;
