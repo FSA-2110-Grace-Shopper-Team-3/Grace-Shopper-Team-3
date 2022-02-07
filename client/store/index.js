faabc27b854da50048c87adb38d5127403a4f5a2
Louis J Rabeno <lrabeno@gmail.com>
2022-02-07 15:20:27 -0500

    added users to store

--- client/store/index.js
+++ client/store/index.js
@@ -1,16 +1,23 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
-import {createLogger} from 'redux-logger'
-import thunkMiddleware from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import auth from './auth'
-import { products } from './products'
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import auth from './auth';
+import { products } from './products';
+import { users } from './users';
 
-const reducer = combineReducers({ auth, products })
+const reducer = combineReducers({
+  auth,
+  products,
+  users,
+});
+
 const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
-)
-const store = createStore(reducer, middleware)
+  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
+);
+const store = createStore(reducer, middleware);
 
-export default store
-export * from './auth'
-export * from './products'
+export default store;
+export * from './auth';
+export * from './products';
+export * from './users';
