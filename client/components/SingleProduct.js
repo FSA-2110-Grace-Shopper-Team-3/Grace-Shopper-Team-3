ea18a597b6749ff8daf4a734f6043bcfb0f96a28
Louis J Rabeno <lrabeno@gmail.com>
2022-02-14 16:22:56 -0500

    add to cart from single product

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import {
+  addOrderItem,
+  editOrderItem,
+  addGuestOrderItem,
+  editGuestOrderItem,
+} from '../store';
+import { v4 as uuidv4 } from 'uuid';
 
 const SingleProduct = () => {
   const { id } = useParams();
+  const currentUserId = useSelector((state) => state.auth.id);
   const singleProduct =
     useSelector((state) =>
       state.products.find((product) => product.id === id)
     ) || {};
+
+  const user = useSelector((state) => state.auth.id);
+  const orders = useSelector((state) => state.orders);
+  const orderItems = useSelector((state) => state.orderItems);
+  const guestCart = useSelector((state) => state.guestOrderItems);
+  const matchOrder =
+    orders.find(
+      (order) => order.userId === user && order.isOrdered === false
+    ) || {};
+  const dispatch = useDispatch();
 
   return (
     <div className="single-prod">
@@ -21,6 +39,56 @@ const SingleProduct = () => {
         <li>Description: {singleProduct.description}</li>
         {/* <li>{singleProduct.category}</li> BACKEND ONLY? */}
       </ul>
+      <button
+        onClick={() => {
+          if (!currentUserId) {
+            const guestOrderItem = guestCart.find(
+              (orderItem) =>
+                orderItem.productId === singleProduct.id &&
+                orderItem.userId === null
+            );
+            guestOrderItem
+              ? dispatch(
+                  editGuestOrderItem({
+                    ...guestOrderItem,
+                    id: guestOrderItem.id,
+                    quantity: guestOrderItem.quantity + 1,
+                  })
+                )
+              : dispatch(
+                  addGuestOrderItem({
+                    productId: singleProduct.id,
+                    userId: null,
+                    id: uuidv4(),
+                    quantity: 1,
+                  })
+                );
+          } else {
+            const orderItem = orderItems.find(
+              (orderItem) =>
+                orderItem.productId === singleProduct.id &&
+                orderItem.orderId === matchOrder.id
+            );
+            orderItem
+              ? dispatch(
+                  editOrderItem({
+                    id: orderItem.id,
+                    quantity: orderItem.quantity + 1,
+                    userId: currentUserId,
+                  })
+                )
+              : dispatch(
+                  addOrderItem({
+                    productId: singleProduct.id,
+                    orderId: matchOrder.id,
+                    userId: currentUserId,
+                  })
+                );
+          }
+        }}
+      >
+        Add to Cart
+      </button>
     </div>
   );
 };
